9d09a99d0a151c2929727b68b2a12d3c2840d54c
NikolayIgnatev <114672912+NikolayIgnatev@users.noreply.github.com>
2025-02-06 16:08:58 +0300

    code file added

---
+++ pythonProject6/main.py
@@ -0,0 +1,32 @@
+def has_digit(password):
+    return any([symbol.isdigit() for symbol in password])
+
+
+def is_very_long(password):
+    return len(password) >= 12
+
+
+def has_upper_letters(password):
+    return any([symbol.isupper() for symbol in password])
+
+
+def has_lower_letters(password):
+    return any([symbol.islower() for symbol in password])
+
+
+def has_symbols(password):
+    return any([not symbol.isdigit() and not symbol.isalpha()
+                for symbol in password])
+
+
+password = input('введите пароль')
+
+score = 0
+functions = [has_digit(password), is_very_long(password),
+             has_upper_letters(password), has_lower_letters(password),
+             has_symbols(password)]
+
+for function in functions:
+    if function:
+        score += 2
+print('Оценка пароля:', score)
